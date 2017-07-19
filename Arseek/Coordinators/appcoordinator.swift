230447ae6e4a61b47f9f4cb635ded0640f6fb7c6
Ian Alexander Rahman <ian.alexander.rahman@gmail.com>
2017-07-19 17:40:53 -0400

    add app coordinator

---
+++ Arseek/Coordinators/appcoordinator.swift
@@ -0,0 +1,12 @@
+//
+//  AppCoordinator.swift
+//  Arseek
+//
+//  Created by Ian Rahman on 7/19/17.
+//  Copyright © 2017 Evergreen Labs. All rights reserved.
+//
+
+import Foundation
+
+// TODO: - Implement
+// https://github.com/ianrahman/Victory/
